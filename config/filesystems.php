8349773ac8162dcc723355a3da91d36ceb7fe7b0
Oliwier Drop <oliwier.drop11@outlook.com>
2026-03-23 12:46:14 +0100

    poprawnienie lokalizacji zapisywania zdjęć

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,7 +40,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => public_path('storage'),
             'url' => rtrim(env('APP_URL', 'http://localhost'), '/').'/storage',
             'visibility' => 'public',
             'throw' => false,
@@ -73,8 +73,6 @@ return [
     |
     */
 
-    'links' => [
-        public_path('storage') => storage_path('app/public'),
-    ],
+    'links' => [],
 
 ];
